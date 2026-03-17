be3491fff1d012cf936e24a8c22277dcffa435f0
tovicito1 <tovictakamine@gmail.com>
2026-03-17 16:31:23 +0100

    Añadir mi segundo proyecto de C

---
+++ proyectos_c_sin_compilar/variables.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  char inicial = 'T';
+  char archivos[] = "Muchos archivos, no quiero contarlos";
+  printf("Hola, tu inicial es %c y tienes %s \n", inicial, archivos);
+  return 0;
+}
